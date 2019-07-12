cbc6c7a796d593ac669547ffbc37b24951cc31d0
Jash Shah <jash.shah@live.com>
2019-07-12 14:45:47 +0530

    Add State model

---
+++ db/migrate/20190712091527_create_states.rb
@@ -0,0 +1,18 @@
+class CreateStates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.flloat :field_1
+      t.float :field_2
+      t.float :field_3
+      t.float :field_4
+      t.float :field_5
+      t.float :field_6
+      t.float :field_7
+      t.float :field_8
+      t.float :field_9
+
+      t.timestamps
+    end
+  end
+end
