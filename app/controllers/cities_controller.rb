7c628c9d4732cd31a2f04984ca5f3adcef1c6ec8
Jash Shah <jash.shah@live.com>
2019-07-24 16:43:04 +0530

    Add Map in City controller and view

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,11 @@
 class CitiesController < ApplicationController
   def index
+  @cities = City.where.not(latitude: nil, longitude: nil)
+    @markers = @cities.map do |city|
+      {
+        lat: city.latitude,
+        lng: city.longitude
+      }
+    end
   end
 end
