28e919fd05fac90703cab0d4f544da4531c1a03b
Jash Shah <jash.shah@live.com>
2019-07-26 17:01:22 +0530

    Final Commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require rails-ujs
 //= require_tree .
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initMapbox } from '../packs/init_map.js';
+
+initMapbox();
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
+  lookup: :nominatim,         # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: MAPBOX_API_KEY='pk.eyJ1IjoibG9naWNpc2FsaWFyIiwiYSI6ImNqeWg1NXVuZDA1OTYzY3BsaXJ4anEwY3IifQ.yM3CC8YIBN6HhVf4Bpi_kg',               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
